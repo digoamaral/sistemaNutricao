6187dd8a7db46db3e532389c43ab3a0107ab8b4b
Rodrigo Amaral Andrade <conta.edcr@gmail.com>
2023-08-18 11:37:46 -0300

    Melhorias no codigo
    Melhorias no codigo

---
+++ sistemaNutricao/pacientes.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <clocale>
+#include "funcoes.cpp"
+
+
+int main(void){
+    setlocale(LC_ALL, "pt_BR");
+    menu();
+    system("pause");
+    system("cls");
+    
+    return EXIT_SUCCESS;
+}
